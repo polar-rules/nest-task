62f65a23ae2b62e67e7ccc88a2b22757bc26802d
Andrii Drozdov <andrii.drozdov@oxygean.com>
2023-11-14 02:55:06 +0000

    [#37] Add help description

--- lib/cli/core/core.help.ts
+++ lib/cli/core/core.help.ts
@@ -40,17 +40,44 @@ export class _Help {
         console.info("Provide a list of tasks names and description.");
         console.info();
         console.info(emptyString.times(3), chalk.default.cyan("<project-name>"), emptyString.times(10), "Optional.");
-        console.info(emptyString.times(28), "In case your `nest-cli.json` have `projects` key defined.");
+        console.info(
+            emptyString.times(28),
+            "In case your",
+            chalk.default.cyan("`nest-cli.json`"),
+            "have",
+            chalk.default.cyan("`projects`"),
+            "key defined.",
+        );
         console.info();
 
-        console.info(chalk.default.cyan("nest-task run --projectName <project-name> --name <name>"));
+        console.info(chalk.default.cyan("nest-task run --projectName <project-name> --name <name> <other-argument>"));
         console.info("Execute the task, can be found by name");
         console.info();
         console.info(emptyString.times(3), chalk.default.cyan("<project-name>"), emptyString.times(10), "Optional.");
-        console.info(emptyString.times(28), "In case your `nest-cli.json` have `projects` key defined.");
+        console.info(
+            emptyString.times(28),
+            "In case your",
+            chalk.default.cyan("`nest-cli.json`"),
+            "have",
+            chalk.default.cyan("`projects`"),
+            "key defined.",
+        );
         console.info(emptyString.times(28), "This argument should come first before name of the task");
+        console.info(emptyString.times(3), chalk.default.cyan("<other-arguments>"), emptyString.times(7), "Optional.");
+        console.info(
+            emptyString.times(28),
+            "In case your you need to pass additional arguments to your task you can easily achieve this with same syntax as you pass <name>",
+        );
+        console.info(emptyString.times(28), "For example:");
+        console.info(emptyString.times(28), chalk.default.cyan('--userId 3 --bio "I\'m awesome"'));
         console.info(emptyString.times(3), chalk.default.cyan("<name>"), emptyString.times(18), "Required.");
-        console.info(emptyString.times(28), "Name of the task defined in @Decorators.Task `name` key");
+        console.info(
+            emptyString.times(28),
+            "Name of the task defined in",
+            chalk.default.cyan("@Decorators.Task"),
+            chalk.default.cyan("`name`"),
+            "key",
+        );
         console.info();
 
         console.info(
@@ -58,30 +85,55 @@ export class _Help {
                 "nest-task create --projectName <project-name> --name <name> --description <description>",
             ),
         );
-        console.info("Run this command as a generator to create task boilerplate, that includes @Runner and @Task.");
         console.info(
-            "Performs 3 basic actions: modify `nest-cli.json`, creates an entrypoint for tasks to run and creates an example of task.",
+            "Run this command as a generator to create task boilerplate, that includes",
+            chalk.default.cyan("@Decorators.Runner"),
+            "and",
+            chalk.default.cyan("@Decorators.Task."),
         );
         console.info();
         console.info(emptyString.times(3), chalk.default.cyan("<project-name>"), emptyString.times(10), "Optional.");
-        console.info(emptyString.times(28), "In case your `nest-cli.json` have `projects` key defined.");
+        console.info(
+            emptyString.times(28),
+            "In case your",
+            chalk.default.cyan("`nest-cli.json`"),
+            "have",
+            chalk.default.cyan("`projects`"),
+            "key defined.",
+        );
         console.info(emptyString.times(3), chalk.default.cyan("<name>"), emptyString.times(18), "Required.");
         console.info(
             emptyString.times(28),
             "Expect to receive a task name, which should converted to file name and class name.",
         );
         console.info(emptyString.times(3), chalk.default.cyan("<description>"), emptyString.times(11), "Required.");
-        console.info(emptyString.times(28), "Short description of task");
+        console.info(
+            emptyString.times(28),
+            "Short description of task. In case if it's a long sentence use",
+            chalk.default.cyan('`"`'),
+            "to screen it.",
+        );
+        console.info(emptyString.times(28), "For example:");
+        console.info(emptyString.times(28), chalk.default.cyan('--description "The task example"'));
         console.info();
 
         console.info(chalk.default.cyan("nest-task setup --projectName <project-name> --convention <convention>"));
-        console.info("Used in purpose to run first initial setup of the @bear-hugs/nest-task.");
+        console.info("Used in purpose to run first initial setup of the", chalk.default.cyan("@bear-hugs/nest-task."));
         console.info(
-            "Performs 3 basic actions: modify `nest-cli.json`, creates an entrypoint for tasks to run and creates an example of task.",
+            "Performs 3 basic actions: modify",
+            chalk.default.cyan("`nest-cli.json`,"),
+            "creates an entrypoint for tasks to run and creates an example of task.",
         );
         console.info();
         console.info(emptyString.times(3), chalk.default.cyan("<project-name>"), emptyString.times(10), "Optional.");
-        console.info(emptyString.times(28), "In case your `nest-cli.json` have `projects` key defined.");
+        console.info(
+            emptyString.times(28),
+            "In case your",
+            chalk.default.cyan("`nest-cli.json`"),
+            "have",
+            chalk.default.cyan("`projects`"),
+            "key defined.",
+        );
         console.info(emptyString.times(3), chalk.default.cyan("<convention>"), emptyString.times(12), "Required.");
         console.info(
             emptyString.times(28),
