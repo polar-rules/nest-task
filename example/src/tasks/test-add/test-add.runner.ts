01b31794154d82886c9dd02714ced2aa5d6dcee6
Andrii Drozdov <andrii.drozdov@oxygean.com>
2023-11-18 05:06:46 +0000

    [#61] Add logger (#65)

--- example/src/tasks/test-add/test-add.runner.ts
+++ example/src/tasks/test-add/test-add.runner.ts
@@ -1,8 +1,14 @@
 import { Abstractions, Decorators } from "@bear-hugs/nest-task";
+import { AppService } from "../../app.service";
 
 @Decorators.Runner()
 export class _Runner extends Abstractions.Runner {
+    constructor(private readonly appService: AppService) {
+        super();
+    }
+
     public async perform(): Promise<void> {
+        console.log("appService", this.appService);
         // Write your code here
     }
 }
