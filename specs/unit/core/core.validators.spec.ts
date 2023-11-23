0549a2281f200cb50ca5c297d6e846e2e7480f99
Andrii Drozdov <andrii.drozdov@oxygean.com>
2023-11-23 04:23:48 +0000

    [#102] Test changes

--- specs/unit/core/core.validators.spec.ts
+++ specs/unit/core/core.validators.spec.ts
@@ -5,15 +5,15 @@ describe("Core::Validators", (): void => {
 
     describe(".Perform", (): void => {
         describe(".validateDependencies", (): void => {
-            xit("Should not raise error if array is empty", (): void => {
+            it("Should not raise error if array is empty", (): void => {
                 expect(() => subject.Perform.validateDependencies([])).not.toThrow();
             });
 
-            xit("Should not raise an error if array have values", (): void => {
+            it("Should not raise an error if array have values", (): void => {
                 expect(() => subject.Perform.validateDependencies([<any>{}, <any>{}])).not.toThrow();
             });
 
-            xit("Should will raise an error if one of the value is nullable", (): void => {
+            it("Should will raise an error if one of the value is nullable", (): void => {
                 expect(() => subject.Perform.validateDependencies([<any>{}, undefined, <any>{}])).toThrowError(
                     Core.Errors.CannotResolveDependencies,
                 );
